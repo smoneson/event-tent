4332b987c7cd3c478b1734357d394cb70d5b1a0d
Simon Erikson <simon.erikson22@gmail.com>
2024-10-10 14:40:33 -0700

    restructure app for App Routing. Add basic API routes for events and users

---
+++ app/api/events/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../lib/prisma";
+
+export async function POST(request: Request) {
+    const data = await request.json();
+    const event = prisma.event.create({ data });
+    return NextResponse.json(event);
+}
+
+export async function GET() {
+    const events = await prisma.event.findMany();
+    return NextResponse.json(events);
+}
